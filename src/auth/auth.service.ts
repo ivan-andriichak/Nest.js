ff98aa06830bfe5771063fe9617480f7766e2892
ivan-andriichak <picea.abies10@gmail.com>
2024-05-21 21:35:54 +0300

    Lesson-2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      defaultModulesExpandDepth: 2,
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port http://localhost:3000');
+    console.log('Swagger is running on port http://localhost:3000/docs');
+  });
 }
 void bootstrap();
